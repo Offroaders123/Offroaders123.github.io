8c4406e03094a823feb8027fc186782331f9e16a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-04-23 11:42:15 -0700

    Self-Showcase!
    Working on learning how to present myself to the world! I realized while writing this, that I still have high tendencies to need to explain myself before I am able to just show the work. I do this in real-life too, prefacing all of my opinions and work with reasons for why it might be right-wrong, and why it might not be something you like-dislike, as well as something you might expect-not expect. I am now starting to see that I don't need to do this. It is distracting once again from that initial impact. I am afraid of how I am seen. I have a voice. I am afraid to use it.
    
    Signpost to the Stars - Feed the Forest (Daisy Rickman)
    
    I am not ashamed of this! Me getting to this point in the first place is progress. All I am looking for is progress. The outcomes come as a result of progress. You can't create outcomes, you can only shape progress in the now.

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,27 @@
 export default function Main() {
   return (
-    <p>Hello world! 👋</p>
+    <>
+      <h4>Hello world! 👋</h4>
+      <p>Hey there! My name is Brandon Bennett.</p>
+
+      <p>I have recently discovered that I am a 'jack of all trades'. That seems to be my single specialty.</p>
+      <p>I am a very creative-driven person. I love listening to music, programming, running, music production, and playing guitar (not in that specific order).</p>
+      <p>I have found that each of my specialties comes in waves, and each one seems to be at the forefront at a given time. I don't know if other people are like this too, but it definitely seems to be how I function.</p>
+      <p>I have recently learned that I think I should follow this strength of mine, even if it doesn't appear as such, depending on the ways you look at it. I haven't tried making this my strength yet, but I now see that it has potential for me.</p>
+      <p>Recently my self confidence has been going upwards in waves. I see what I am capable of, and I am now proud of it. I used to be timid to state that I am all of these things, because I felt I would come across as boastful or self-indulgent. My self-awareness was keeping me from sharing my personal light with others.</p>
+      <p>I am a programmer, a mountain biker, guitar player, distance runner, tech enthusiast, and an empath. I seem to feel deeply, but not know how to show it, or <s>am</s> <em>have been</em> too uncomfortable to show it. Now I see that I am indeed capable of being honest and open with others. I can already do it with myself I feel, so I have no reasons to keep from being that same way with others.</p>
+
+      <p>Over the last few years, I have delved far into my interest in music, by taking part in the creation process of it. There have been many ups and downs, and I am thankful for the entire part of it to get to where I am now. I wouldn't be who or where I am now without that of the successess, failures, missteps, and luck that happened along the way.</p>
+      <p>I am now very proud of everything I have been able to catalog with my music. It very much opens my eyes to even further what I may be capable with these abilities that I have. We all have our own specialties. What is important to me might not be for you. What is important to you might not be important to me. That's the beauty of it.</p>
+      <p>My very deep goal would be to help people in similar situations that I've struggled with, to also be able to see the light for what they are, and be able to stand by their own side and believe in themselves as well. This has to come from yourself for you though. That's the tough part. It's not for namesake or popularity, it comes from a place of self love and care.</p>
+
+      <p>I am not as grandiose as I may be sounding here. It is very much how I feel inside though. That's why I have to articulate it. Sometimes your deep thoughts like those feel unrealistic or too good to be true, or like you are too above yourself. In that mindset, it is sickening to be honest. But now I see, being open in that way allows you to let go of that mental strife, open up the recesses of your mind for new musings and things to learn. There's lots of room for growth, all of the time.</p>
+
+      <p>My music has very many different avenues and intentions, and trying to explain them all won't do it justice. It will end up in paragraphs like the parts above. I don't intent to do that today, as that's not what this is about. I could write all day. I am going to restrain myself from doing that, as this initially has the intention to solely represent everything that I am proud of. I don't need to explain it. That, I <em>do</em> still need to work on.</p>
+
+      <p>I am proud of myself, so I am going to show you it!</p>
+
+      <p><em>TBD... (probably next commit)</em></p>
+    </>
   );
 }
