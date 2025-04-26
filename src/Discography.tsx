0ff6bf1b63eab82538e194f3703d422ba3b4dfe1
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-04-26 02:53:32 -0700

    Discography Basic Grid Styling
    I have yet to properly remember how to setup a CSS grid lol. Had to get some help from GPT for this one. I am more of a flexbox guru myself. I love grid though, in concept. I am looking to change that now.

--- src/Discography.tsx
+++ src/Discography.tsx
@@ -2,6 +2,7 @@ import { createResource, For, type ResourceReturn } from "solid-js";
 import bandcampDataPath from "/bandcamp-data.json?url";
 import type { Release as BandcampRelease } from "./bandcamp.ts";
 import Release from "./Release.tsx";
+import "./Discography.css";
 
 export default function Discography() {
   const [bandcampData]: ResourceReturn<BandcampRelease[], unknown> = createResource(async () => {
@@ -10,7 +11,7 @@ export default function Discography() {
   });
 
   return (
-    <div>
+    <div class="Discography">
       <For each={bandcampData()}>
         {release => (
           <Release
