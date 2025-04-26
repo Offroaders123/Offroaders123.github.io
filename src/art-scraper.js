f80397aeff96dd2b08f0c128d0bbde9a8acdd8af
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-04-26 02:42:19 -0700

    Release Art Naming Scheme
    Some of the names don't work as file paths when fetched (maybe it's Vite specifically?), so I am renaming these to strings that are more URL-friendly. Bandcamp already does this for their URLs, I realized I could just use those instead of the full names that have those unexpected special characters.

--- src/art-scraper.js
+++ src/art-scraper.js
@@ -1,3 +1,3 @@
 // Run this in DevTools at `https://rbnaodn.bandcamp.com/`
 
-JSON.stringify([...document.querySelectorAll("#music-grid .music-grid-item")].map(item => [item.querySelector("a p").innerText, item.querySelector("img").src]), null, 2);
+JSON.stringify([...document.querySelectorAll("#music-grid .music-grid-item")].map(item => [item.querySelector("a").href.split("/").at(-1), item.querySelector("img").src]), null, 2);
