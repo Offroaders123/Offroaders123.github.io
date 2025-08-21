4b839cb3cf10c7870681cdbeef1c0b3cf5b6ad49
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-08-21 14:06:10 -0700

    Summer Releases Update
    Recreational Outrage, and others!
    
    Really loving this project (the new album). This site is great too, I am currently slowly but surely working on being able to have the drive the share things I am proud of though. I think I still have the skepticism to outright share things with others. I am able to make them public, but I seem to still have unsuredness about showing them outright. It is getting stronger though, and that's progress! Showed some songs I've been working on with others today. That's a new one for me, and I wasn't nervous either. Insane growth. Keep it up, "king"! I feel like that is cheesy when people say that.

--- src/art-scraper.js
+++ src/art-scraper.js
@@ -1,3 +1,5 @@
 // Run this in DevTools at `https://rbnaodn.bandcamp.com/`
+// be sure to scroll down and load all of the image before running
+// lazy loading prevents them from populating before you can properly access them
 
 JSON.stringify([...document.querySelectorAll("#music-grid .music-grid-item")].map(item => [item.querySelector("a").href.split("/").at(-1), item.querySelector("img").src]), null, 2);
