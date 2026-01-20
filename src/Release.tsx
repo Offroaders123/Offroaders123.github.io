4820a44654dbb977c698f8fe105d1ce1b46499eb
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2026-01-20 00:26:25 -0800

    Release Dialog Modal
    Setting up for adding descriptions from Bandcamp. Having dialogs is the current way I see to maybe making it work. Not the best of UX, nor too pretty, but I will come back to that later on. Just want the info there and accessible more easily than it currently is. And having this in more than one place is great too.

--- src/Release.tsx
+++ src/Release.tsx
@@ -1,18 +1,50 @@
+import { createEffect, createSignal, type Signal } from "solid-js";
 import type { MusicLike } from "./bandcamp.ts";
+import "./Release.css";
 
 export interface ReleaseProps extends Pick<MusicLike, "title" | "publish_date" | "release_date" | "page_url"> { }
 
 export default function Release(props: ReleaseProps) {
+  let dialog: HTMLDialogElement;
+  const [showDialog, setShowDialog]: Signal<boolean> = createSignal<boolean>(false);
+
+  const imgSrc: string = `/${props.page_url.split("/").at(-1)}.jpg`;
+
+  createEffect(() => {
+    if (showDialog()) {
+      dialog!.showModal();
+    }
+  });
+
   return (
-    <div>
-      <img
-        width={168}
-        height={168}
-        src={`/${props.page_url.split("/").at(-1)}.jpg`}
-        alt={props.title}
-        loading="lazy"
-      />
-      <p><a href={`https://rbnaodn.bandcamp.com${props.page_url}`}>{props.title}</a></p>
-    </div>
+    <>
+      <button
+        class="Release"
+        onclick={() => setShowDialog(true)}>
+        <img
+          width={168}
+          height={168}
+          src={imgSrc}
+          alt={props.title}
+          loading="lazy"
+        />
+        <p>{props.title}</p>
+      </button>
+      <dialog
+        ref={dialog!}
+        onclose={() => setShowDialog(false)}>
+        <form method="dialog">
+          <img
+            width={350}
+            height={350}
+            src={imgSrc}
+            alt={props.title}
+            loading="lazy"
+          />
+          <p><a href={`https://rbnaodn.bandcamp.com${props.page_url}`}>{props.title}</a></p>
+          <button type="submit">Close</button>
+        </form>
+      </dialog>
+    </>
   );
 }
