cf837aa329de6cc5a6b37ba9b05b2233140a41af
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2026-01-29 20:57:13 -0800

    Consolidate Function Definition
    Okay, now it's no longer scope, it's functional! Yay.
    
    Just listened to Wannafaratakow (Teeth Critter). Man, I love how that song turned out. Ferret Train really is a great album. Very daunting.
    
    Listening to Integrate now.
    
    I do think I want to use a little more master compression in general, at least for the ambient projects. But then again, I am listening with Mac speakers right now, and it definitely feels like it's meant to be a headphone listen. So whatever works though.
    
    I love music, wow. I know it's cheeky to love your own stuff, to some extent, but man I really am proud of what I have made, and what I am capable of making. I can't wait to see what I make going forwards. I can totally pick up on a bunch of the inspirations that led me to where I am right now.
    
    I love Integrate. It is meant to be a bit loud and overbearing in some points, so maybe the mastering is okay. It's like waves of bright light that wash over you, and it is beautiful, yet immense. So it is a bit much.

--- src/consolidate.ts
+++ src/consolidate.ts
@@ -1,22 +1,17 @@
 import type { LyricsInfo, ParserResult, Release as ParserRelease, TrAlbum, TrackInfo } from "./bandcamp-parser.ts";
-import type { Release } from "./discography.ts";
+import type { Release, Track } from "./discography.ts";
 
-declare const discographyData: ParserResult;
-
-// `consolidated` is valid against the type `Release[]` from `discography.ts`.
-// It merely doesn't pass because `type` is `string` vs `"album" | "track"`.
-// Moving to solid types will help mitigate this.
-// This can probably be a general function inside of `bandcamp-parser`, now
-// that everything is fully type-safe both directions.
-// I tested everything against the old `discography-old.json` file (non-consolidated).
-const consolidated: Release[] = discographyData.releases.map((release: ParserRelease, i: number) => {
-  const albumTracks: TrAlbum = discographyData.albumsTracks[i]!;
-  const lyricsInfos: LyricsInfo[] = discographyData.lyricsInfos[i]!;
-  return {
-    ...release,
-    ...albumTracks,
-    tracksInfo: albumTracks.tracksInfo.map(({ lyrics: _lyrics, ...info }: TrackInfo, i: number) => ({ ...info, ...lyricsInfos[i]! }))
-  };
-});
-
-console.dir(consolidated, { depth: null });
+export function consolidateResult(discographyData: ParserResult): Release[] {
+  return discographyData.releases.map((release: ParserRelease, i: number): Release => {
+    const albumTracks: TrAlbum = discographyData.albumsTracks[i]!;
+    const lyricsInfos: LyricsInfo[] = discographyData.lyricsInfos[i]!;
+    return {
+      ...release,
+      ...albumTracks,
+      tracksInfo: albumTracks.tracksInfo
+        .map(({ lyrics: _lyrics, ...info }: TrackInfo, i: number): Track =>
+          ({ ...info, ...lyricsInfos[i]! })
+        )
+    };
+  });
+}
