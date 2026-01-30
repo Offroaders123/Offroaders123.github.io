1b9d8d1f0276c872295d30b33bc0cfcffd260901
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2026-01-29 22:07:38 -0800

    Separated Release Components
    Lol, `type Setter` 💅
    
    Making things more modular as it allows for hot reload.

--- src/Release.tsx
+++ src/Release.tsx
@@ -1,20 +1,14 @@
-import { createEffect, createSignal, type Signal } from "solid-js";
+import { createSignal, type Signal } from "solid-js";
 import type { Release } from "./discography.ts";
+import ReleaseDialog from "./ReleaseDialog.tsx";
 import "./Release.css";
 
-export type ReleaseProps = Pick<Release, "title" | "about" | "page_url">;
+export interface ReleaseProps extends Pick<Release, "title" | "about" | "page_url"> { }
 
 export default function Release(props: ReleaseProps) {
-  let dialog: HTMLDialogElement;
   const [showDialog, setShowDialog]: Signal<boolean> = createSignal<boolean>(false);
 
   const imgSrc: string = `/${props.page_url.split("/").at(-1)}.jpg`;
-
-  createEffect(() => {
-    if (showDialog()) {
-      dialog!.showModal();
-    }
-  });
 
   return (
     <>
@@ -30,23 +24,12 @@ export default function Release(props: ReleaseProps) {
         />
         <p>{props.title}</p>
       </button>
-      <dialog
-        class="Release-Dialog"
-        ref={dialog!}
-        onclose={() => setShowDialog(false)}>
-        <form method="dialog">
-          <img
-            width={350}
-            height={350}
-            src={imgSrc}
-            alt={props.title}
-            loading="lazy"
-          />
-          <p><a href={`https://rbnaodn.bandcamp.com${props.page_url}`}>{props.title}</a></p>
-          <pre>{props.about}</pre>
-          <button type="submit">Close</button>
-        </form>
-      </dialog>
+      <ReleaseDialog
+        {...props}
+        showDialog={showDialog}
+        setShowDialog={setShowDialog}
+        imgSrc={imgSrc}
+      />
     </>
   );
 }
---
+++ src/ReleaseDialog.tsx
@@ -0,0 +1,39 @@
+import { type Accessor, createEffect, type Setter } from "solid-js";
+import type { ReleaseProps } from "./Release.tsx";
+import "./ReleaseDialog.css";
+
+export interface ReleaseDialogProps extends ReleaseProps {
+  showDialog: Accessor<boolean>;
+  setShowDialog: Setter<boolean>;
+  imgSrc: string;
+};
+
+export default function ReleaseDialog(props: ReleaseDialogProps) {
+  let dialog: HTMLDialogElement;
+
+  createEffect(() => {
+    if (props.showDialog()) {
+      dialog!.showModal();
+    }
+  });
+
+  return (
+    <dialog
+      class="Release-Dialog"
+      ref={dialog!}
+      onclose={() => props.setShowDialog(false)}>
+      <form method="dialog">
+        <img
+          width={350}
+          height={350}
+          src={props.imgSrc}
+          alt={props.title}
+          loading="lazy"
+        />
+        <p><a href={`https://rbnaodn.bandcamp.com${props.page_url}`}>{props.title}</a></p>
+        <pre>{props.about}</pre>
+        <button type="submit">Close</button>
+      </form>
+    </dialog>
+  );
+}
